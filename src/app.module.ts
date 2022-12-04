c06783d75cb2c68428ee769f9544398955681055
NatanSzymczak <natan.szymczak@gmail.com>
2022-12-04 15:47:20 +0100

    App module configuration

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DataSource } from 'typeorm';
+import { ReadingsModule } from './readings/readings.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      synchronize: true,
+      entities: ['**/*.entity.js'],
+    }),
+    ReadingsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
