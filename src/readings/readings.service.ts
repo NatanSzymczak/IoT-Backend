f267a19297e8c7fd7b0c0121aed3d22ed5f20d90
NatanSzymczak <natan.szymczak@gmail.com>
2022-12-21 09:25:06 +0100

    Add simulator function

--- src/readings/readings.service.ts
+++ src/readings/readings.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/users.entity';
 import { Repository } from 'typeorm';
 import { Reading } from './readings.entity';
 
@@ -13,7 +14,22 @@ export class ReadingsService {
 
   async add() {}
 
-  async generator() {}
-
-  async deviceSimulatorUser5Id() {}
+  async deviceSimulatorUser5Id() {
+    let hour = 0;
+    const simulator = () => {
+      const user = new User();
+      user.id = 2;
+      const newReading = new Reading();
+      newReading.user = user;
+      // newReading.timestamp = `01/12/2022 ${hour < 10 ? `0${hour}` : `${hour}`}:30:00`;
+      newReading.temperature = Math.round(Math.random() * (55 - 45) + 45);
+      newReading.pressure = Math.round(Math.random() * (55 - 45) + 45);
+      newReading.flow = Math.round(Math.random() * (65 - 55) + 55);
+      newReading.pH = +(Math.random() * (7.4 - 6.6) + 6.6).toFixed(1);
+      hour++;
+      console.log(hour);
+      this.repo.save(newReading);
+    };
+    await setInterval(simulator, 5000);
+  }
 }
