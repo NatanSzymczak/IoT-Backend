659c1512c82106780916691102457c9621c6e393
NatanSzymczak <natan.szymczak@gmail.com>
2023-01-11 20:04:54 +0100

    Create function to add new user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -8,5 +9,10 @@ export class UsersController {
   @Get()
   getUsers() {
     return this.usersService.getAll();
+  }
+
+  @Post()
+  addUser(@Body() body: CreateUserDto) {
+    return this.usersService.add(body.firstName, body.lastName, body.isActive);
   }
 }
