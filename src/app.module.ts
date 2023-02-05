8e0bf2d10bfbe32e93230dae462de02f2062484d
NatanSzymczak <natan.szymczak@gmail.com>
2023-02-05 21:17:13 +0100

    App module config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DataSource } from 'typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { ReadingsModule } from './readings/readings.module';
 import { UsersModule } from './users/users.module';
 
